18cee279009ec2eeea305a44850a1a94b4f07015
bfrasure <bill@solutionreach.com>
2016-10-15 18:56:57 -0600

    Start speccing out RoutingSystem  #time 30m

---
+++ src/main/java/com/example/RoutingSystem.java
@@ -0,0 +1,26 @@
+package com.example;
+
+public abstract class RoutingSystem {
+    // Calculate total time spent transferring items between methods of transportation
+    private int transferPenalty;
+    private double shrinkageFactor;
+
+    double routeItems(int numItems, int numTransfers) {
+        return (numItems * shrinkageFactor) + (numTransfers * transferPenalty);
+    }
+}
+
+class HumanRouters extends RoutingSystem {
+    private int transferPenalty = 2;
+    private double shrinkageFactor = 1.3;
+};
+
+class MonkeyRouters extends RoutingSystem {
+    private int transferPenalty = 1;
+    private double shrinkageFactor = 2.0;
+};
+
+class ComputerRouters extends RoutingSystem {
+    private int transferPenalty = 5;
+    private double shrinkageFactor = 1.0;
+};
